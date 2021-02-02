f129c4728a2dc323829608baa69fd30537faf7ef
Lydia <39285530+qiaor1@users.noreply.github.com>
2021-02-01 22:19:09 -0800

    tweet

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -22,6 +22,9 @@ class HomeTableViewController: UITableViewController {
         tableView.refreshControl = myRefreshControl
         
     }
+    override func viewDidAppear(_ animated: Bool) {
+        loadTweets()
+    }
     @objc func loadTweets(){
         numberOfTweet = 20
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
